51ccca22df6a0aa8ffdadba0cde96021032d3d9f
Rayan916 <officialdevrayan@gmail.com>
2026-08-12 19:30:46 +0500

    Update ImageStego.jsx

--- src/pages/ImageStego.jsx
+++ src/pages/ImageStego.jsx
@@ -414,7 +414,7 @@ function ImageStego() {
                                 src={selectedImage}
                                 alt="preview"
                                 className={Styles.previewImage}
-                            />   
+                            />  
                              
                         ) : (
                         
